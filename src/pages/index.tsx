ad66a9a7e896ec761f2644c087d7ad7fbea1c0e8
Vince-html <82712898+Vince-html@users.noreply.github.com>
2021-11-10 15:37:03 -0300

    Desafio finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
@@ -8,7 +9,37 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type fetchImagesResponse = {
+  data: Card[];
+  after: string | null;
+};
+
 export default function Home(): JSX.Element {
+  async function fetchImages({
+    pageParam = null,
+  }): Promise<fetchImagesResponse> {
+    if (pageParam) {
+      const { data } = await api.get(`/api/images`, {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return data;
+    }
+    const { data } = await api.get(`/api/images`);
+
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -16,20 +47,28 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage.after ?? null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    let formattedDataTotal = [] as Card[];
+    const dataPages = data?.pages;
+
+    dataPages?.map(page => {
+      formattedDataTotal = [...formattedDataTotal, ...page.data];
+    });
+
+    return formattedDataTotal;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +76,16 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            mt="1rem"
+            onClick={() => fetchNextPage()}
+            role="button"
+            w={['100%', 'auto']}
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
